345c7d735c52da9df5742af6e6ef23550d7664f6
V473R10 <fac.valerio@gmail.com>
2023-02-03 22:33:54 -0300

    Enable connection pooling

--- Ktor/tutorial-website/build.gradle.kts
+++ Ktor/tutorial-website/build.gradle.kts
@@ -5,6 +5,9 @@ val logback_version: String by project
 val exposed_version: String by project
 val h2_version: String by project
 
+val hikaricp_version: String by project
+val ehcache_version: String by project
+
 plugins {
     kotlin("jvm") version "1.8.10"
     id("io.ktor.plugin") version "2.2.3"
@@ -34,6 +37,9 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
     implementation("com.h2database:h2:$h2_version")
+
+    implementation("com.zaxxer:HikariCP:$hikaricp_version")
+    implementation("org.ehcache:ehcache:$ehcache_version")
 
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
--- Ktor/tutorial-website/src/main/kotlin/com/example/dao/DatabaseFactory.kt
+++ Ktor/tutorial-website/src/main/kotlin/com/example/dao/DatabaseFactory.kt
@@ -1,19 +1,34 @@
 package com.example.dao
 
 import com.example.models.*
+import com.zaxxer.hikari.*
+import io.ktor.server.config.*
+import java.io.*
 import kotlinx.coroutines.*
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.*
 import org.jetbrains.exposed.sql.transactions.experimental.*
 
 object DatabaseFactory {
-    fun init() {
-        val driverClassName = "org.h2.Driver"
-        val jdbcURL = "jdbc:h2:file:./build/db"
-        val database = Database.connect(jdbcURL, driverClassName)
-        transaction {
-            SchemaUtils.create(Articles)
-        }
+    private fun createHikariDataSource(
+        url: String,
+        driver: String
+    ) = HikariDataSource(HikariConfig().apply {
+        driverClassName = driver
+        jdbcUrl = url
+        maximumPoolSize = 3
+        isAutoCommit = false
+        transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+        validate()
+    })
+
+    fun init(config: ApplicationConfig) {
+        val driverClassName = config.property("storage.driverClassName").getString()
+        val jdbcURL = config.property("storage.jdbcURL").getString() +
+                (config.propertyOrNull("storage.dbFilePath")?.getString()?.let {
+                    File(it).canonicalFile.absolutePath
+                } ?: "")
+        val database = Database.connect(createHikariDataSource(url = jdbcURL, driver = driverClassName))
     }
 
     suspend fun <T> dbQuery(block: suspend() -> T): T =
